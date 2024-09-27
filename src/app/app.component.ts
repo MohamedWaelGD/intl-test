777b917228d1d06031a5ea05b5e43febbe391c1f
Bassant Khattab <bassantkhattab09@gmail.com>
2024-09-27 17:27:25 +0300

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import intlTelInput from 'intl-tel-input';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,19 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'intl-test';
+
+  constructor() {
+    
+  }
+
+  ngAfterViewInit() {
+    const input = document.querySelector("#phone") as any;
+    const intl = intlTelInput(input, {
+        utilsScript: "intl-tel-input/build/js/utils.js"
+    });
+
+    console.log(intl);
+  }
 }
